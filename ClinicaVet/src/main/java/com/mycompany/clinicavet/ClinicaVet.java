21b70ff5dbe19577593c1fd36393b2c4b9b03175
Carolina <ana.carolina16@aluno.ifsertao-pe.edu.br>
2025-02-22 21:06:02 -0300

    Criação

---
+++ ClinicaVet/src/main/java/com/mycompany/clinicavet/ClinicaVet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicavet;
+
+/**
+ *
+ * @author carlo
+ */
+public class ClinicaVet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
